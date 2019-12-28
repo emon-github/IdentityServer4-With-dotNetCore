1037e78966951d92d35e07abeb331046be0e32ee
Ashraf <az.emon@hotmail.com>
2019-12-28 19:52:08 +0800

    commit web api project

---
+++ BankOfDotnet/BankOfDotnet.API/Models/BankContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotnet.API.Models
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> option): base(option)
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
