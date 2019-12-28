86914522588c889a9b60c1fd82b95b83e813b461
Ashraf <az.emon@hotmail.com>
2019-12-28 23:24:40 +0800

    complete identity server configuration

---
+++ BankOfDotnet/BankOfDotnet.IdentitySrv/Config.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotnet.IdentitySrv
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+            new ApiResource("bankOfDotnet","Customer Api for BankOfDotnet")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+              new  Client
+              {
+                  ClientId="Client",
+                  AllowedGrantTypes= GrantTypes.ClientCredentials,
+                  ClientSecrets=
+                  {
+                  new Secret("Secret".Sha256())
+                  },
+                  AllowedScopes={ "bankOfDotnet" }
+              }
+            };
+        }
+    }
+}
